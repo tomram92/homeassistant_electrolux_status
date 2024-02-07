b2153c43ae4aad49c0fc6bd1225248cb37590ce2
Tomas Ramanauskas <tomas@hotmail.lt>
2024-02-08 00:45:13 +0200

    FIX 6

--- custom_components/electrolux_status/switch.py
+++ custom_components/electrolux_status/switch.py
@@ -1,5 +1,6 @@
 """Switch platform for Electrolux Status."""
 from homeassistant.components.switch import SwitchEntity
+from .electroluxwrapper.oneAppApi import OneAppApi
 
 from .const import SWITCH, DOMAIN
 from .entity import ElectroluxEntity
@@ -15,7 +16,8 @@ async def async_setup_entry(hass, entry, async_add_entities):
     if appliances is not None:
         for appliance_id, appliance in appliances.appliances.items():
             entities = [entity for entity in appliance.entities if entity.entity_type == SWITCH]
-            _LOGGER.debug("Electrolux add %d sensors to registry for appliance %s", len(entities), appliance_id)
+            _LOGGER.debug("Electrolux add %d sensors to registry for appliance %s", len(entities), 
+appliance_id)
             async_add_entities(entities)
 
 
@@ -28,14 +30,33 @@ class ElectroluxSwitch(ElectroluxEntity, SwitchEntity):
         value = self.extract_value()
         if value is None:
             return self._cached_value
+        elif value == "On" or value == "Start":
+            self._cached_value = value
         else:
             self._cached_value = value
         return value
 
     async def async_turn_on(self, **kwargs):
         """Turn the entity on."""
-        _LOGGER.error("Not supported yet")
+        client: OneAppApi = self.api
+        command: dict[str, any] = {}
+        if self.entity_source:
+            command = {self.entity_source: {self.entity_attr: self.extract_value()}}
+        else:
+            command = {self.entity_attr: self.extract_value()}
+        _LOGGER.debug("Electrolux select option %s", json.dumps(command))
+        result = await client.execute_appliance_command(self.pnc_id, command)
+        _LOGGER.debug("Electrolux select option result %s", result)
 
     async def async_turn_off(self, **kwargs):
         """Turn the entity off."""
-        _LOGGER.error("Not supported yet")
+        client: OneAppApi = self.api
+        command: dict[str, any] = {}
+        if self.entity_source:
+            command = {self.entity_source: {self.entity_attr: self.extract_value()}}
+        else:
+            command = {self.entity_attr: self.extract_value()}
+        _LOGGER.debug("Electrolux select option %s", json.dumps(command))
+        result = await client.execute_appliance_command(self.pnc_id, command)
+        _LOGGER.debug("Electrolux select option result %s", result)
+
